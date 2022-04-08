f5eda6aa6dec2e5d4cdaa26c617aeb1e152de192
tokotaru <97116445+tokotaru@users.noreply.github.com>
2022-04-08 21:35:04 +0300

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -11,5 +11,7 @@ class AMod(loader.Module):
             a=message.text.split(" ")
             bashCommand = "pip3 install"+a[1]
             process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            await message.edit("жди 10 секунд и получишь вывод")
+            time.sleep(10)
             output, error = process.communicate()
             await message.edit("сделано,мой повелитель,вот вывод:\n {}".format(output))
