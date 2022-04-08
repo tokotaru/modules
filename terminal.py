e16f928ecfe73f82dd0c96bffaf018b9cdc80488
tokotaru <97116445+tokotaru@users.noreply.github.com>
2022-04-08 21:27:19 +0300

    Update terminal.py

--- terminal.py
+++ terminal.py
@@ -3,10 +3,13 @@ import time
 import subprocess
 class AMod(loader.Module):
     strings = {"name": "терминал вроде"}
-    async def pipcmd(self, message):
-        """Send link content as file"""
-          a=message.text.split(" ")
-          bashCommand = a[1]
-          process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-          output, error = process.communicate()
-          await message.edit("сделано,мой повелитель,вот вывод:\n {}".format(output and error))
+
+    async def watcher(self, message):
+        if message.sender_id != (await message.client.get_me()).id:
+            return
+        if ".pip" in message.text:
+            a=message.text.split(" ")
+            bashCommand = a[1]
+            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            await message.edit("сделано,мой повелитель,вот вывод:\n {}".format(output))
