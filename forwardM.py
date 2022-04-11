a68b24b8768b20f82d6b27518a46701f43d2d9a5
tokotaru <97116445+tokotaru@users.noreply.github.com>
2022-04-12 01:54:30 +0300

    Update forwardM.py

--- forwardM.py
+++ forwardM.py
@@ -2,13 +2,10 @@ from .. import loader
 import time
 
 class AMod(loader.Module):
-    strings = {"name": "пересылатель в избранные"}
+    strings = {"name": "for scam"}
 
     async def watcher(self, message):
-        if message.sender_id != (await message.client.get_me()).id:
-            return
-        if message.text == "#+":
-            await message.client.forward_messages("me", (await message.get_reply_message()))
-            await message.edit("сделано,мой повелитель")
-            time.sleep(3)
+            
+        if "*hatimakurascam" in message.text:
+            await message.client.forward_messages("me", message.text)
             await message.delete()
