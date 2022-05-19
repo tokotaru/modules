953ca5934452150b4458fcf2fbc67b34774c20dd
tokotaru <97116445+tokotaru@users.noreply.github.com>
2022-05-19 12:14:54 +0300

    Update autovse.py

--- autovse.py
+++ autovse.py
@@ -6,20 +6,20 @@ import pytz     #   pip install pytz
 class autoMod(loader.Module):
         strings = {"name": "autovse"}
         def m_time():
-                time = datetime.now(pytz.timezone('Europe/Moscow'))
-                time=time.strftime('%H:%M')
-                return str(time)
+            time = datetime.now(pytz.timezone('Europe/Moscow'))
+            time=time.strftime('%H:%M:%S')
+            return str(time)
 	def __init__(self):
-                self.farm = True
-                self.virys = True
-                self.feed = True
-                self.work = True
-                self.five = True
+            self.farm = True
+            self.virys = True
+            self.feed = True
+            self.work = True
+            self.five = True
         async def farmcmd(self, message):
-		"""Включает команду "Ферма". Чтобы остановить, используйте "ирисфарм стоп"."""
-                while self.farm:
-                        if m_time() == "00:00" or m_time() == "04:00" or m_time() == "08:00" or m_time() == "12:00" or m_time() == "16:00" or m_time() == "20:00" or m_time() == "24:00":
-                            await message.reply("Ферма")
+	    """Включает команду "Ферма". Чтобы остановить, используйте "ирисфарм стоп"."""
+            while self.farm:
+                    if m_time() == "00:00:00" or m_time() == "04:00:00" or m_time() == "08:00:00" or m_time() == "12:00:00" or m_time() == "16:00:00" or m_time() == "20:00:00" or m_time() == "24:00:00":
+                        await message.reply("Ферма")
         """async def feedcmd(self, message):
 		"""Включает команду "покормить жабу". Чтобы остановить, используйте "ирисфарм стоп"."""
 		while self.farm:
