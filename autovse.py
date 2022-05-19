ee6fde8aca0e26972083bf03de35d5756ae3c1ac
tokotaru <97116445+tokotaru@users.noreply.github.com>
2022-05-19 12:21:05 +0300

    Update autovse.py

--- autovse.py
+++ autovse.py
@@ -16,7 +16,7 @@ class autoMod(loader.Module):
         self.work = True
         self.five = True
     async def farmcmd(self, message):
-	"""Включает команду "Ферма". Чтобы остановить, используйте "ирисфарм стоп"."""
+        """Включает команду "Ферма". Чтобы остановить, используйте "ирисфарм стоп"."""
         while self.farm:
                 if m_time() == "00:00:00" or m_time() == "04:00:00" or m_time() == "08:00:00" or m_time() == "12:00:00" or m_time() == "16:00:00" or m_time() == "20:00:00" or m_time() == "24:00:00":
                     await message.reply("ферма")
