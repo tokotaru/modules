5c223ffe3779a9195ce61cc79f0318d5ac20acf6
tokotaru <97116445+tokotaru@users.noreply.github.com>
2022-05-19 11:25:08 +0300

    Update autovse.py

--- autovse.py
+++ autovse.py
@@ -4,8 +4,8 @@ from datetime import datetime
 import pytz     #   pip install pytz
 #модуль не готов
 class autoMod(loader.Module):
-	strings = {"name": "autovse"}
-	def m_time():
+        strings = {"name": "autovse"}
+        def m_time():
                 time = datetime.now(pytz.timezone('Europe/Moscow'))
                 time=time.strftime('%H:%M')
                 return str(time)
@@ -15,7 +15,7 @@ class autoMod(loader.Module):
                 self.feed = True
                 self.work = True
                 self.five = True
-	async def farmcmd(self, message):
+        async def farmcmd(self, message):
 		"""Включает команду "Ферма". Чтобы остановить, используйте "ирисфарм стоп"."""
                 while self.farm:
                         if m_time() == "00:00" or m_time() == "04:00" or m_time() == "08:00" or m_time() == "12:00" or m_time() == "16:00" or m_time() == "20:00" or m_time() == "24:00":
@@ -25,7 +25,7 @@ class autoMod(loader.Module):
 		while self.farm:
 			await message.reply("покормить жабу")
 			await sleep(14500)"""
-        async def watcher(self, message):
+        """async def watcher(self, message):
             me = (await message.client.get_me())
             if message.sender_id == me.id:
                 if message.text.lower() == "ирисфарм стоп":
@@ -33,4 +33,4 @@ class autoMod(loader.Module):
                     await message.reply("<b>Ирисфарм остановлен.</b>")
                 if message.text.lower() == "ирисвирус стоп":
                     self.virys = False
-                    await message.reply("<b>Ирисвирус остановлен.</b>")
+                    await message.reply("<b>Ирисвирус остановлен.</b>")"""
